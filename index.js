a052914d969fe119becbd77fef8ad1042c6dbde6
shanto869 <shantobiswas869@gmail.com>
2022-10-27 07:57:45 +0600

    index file code organize completed

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 
+
 const app = express();
 app.use(cors())
 
@@ -14,23 +15,19 @@ app.get('/', (req, res) => {
     res.send('Online Learning Platform Server');
 })
 
-
 app.get('/courses', (req, res) => {
     res.send(courses)
 })
 
-
 app.get('/course', (req, res) => {
     res.send(coursesDetails)
 })
-
 
 app.get('/course/:id', (req, res) => {
     const id = req.params.id;
     const selected = coursesDetails.find(course => course.id === id)
     res.send(selected)
 })
-
 
 app.listen(port, () => {
     console.log('online learning platform is runnnig is', port)
